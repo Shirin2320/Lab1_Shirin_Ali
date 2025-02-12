23def13c89103197db943262b1b94252bc08295d
Shirin2320 <144370179+Shirin2320@users.noreply.github.com>
2025-02-12 11:20:53 -0500

    commit #20 for the code

--- Lab1_Shirin_Ali/Lab1_Shirin_Ali/ContentView.swift
+++ Lab1_Shirin_Ali/Lab1_Shirin_Ali/ContentView.swift
@@ -133,4 +133,9 @@ struct ContentView: View {
             }
         }
     struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+                ContentView()
+            }
+        }
+
         
